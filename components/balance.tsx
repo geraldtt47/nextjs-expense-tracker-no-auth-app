5f64042db53466ce4e03448a061b1280ade25701
GeraldTT47 <geraldtt47@gmail.com>
2024-07-08 18:07:30 +0200

    refactor: Update background color of balance card in Balance component

--- components/balance.tsx
+++ components/balance.tsx
@@ -42,7 +42,7 @@ export default function Balance() {
     <div className="flex flex-col items-center">
       <div className="flex flex-col md:flex-row gap-4 mt-4">
         <div className="flex-1">
-          <Card className="w-[350] text-center bg-primary text-white">
+          <Card className="w-[350] text-center bg-blue-500 text-white">
             <CardHeader>
               <CardDescription className="font-extrabold text-white">
                 BANK ACCOUNT BALANCE
