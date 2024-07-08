1636e6e8d26e4590caaa864b49209fbdfc34a428
GeraldTT47 <geraldtt47@gmail.com>
2024-07-08 17:55:10 +0200

    feat: Add lazy loading for images in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-
-//const inter = Inter({ subsets: ["latin"] });
+import { ThemeProvider } from "@/components/theme-provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
